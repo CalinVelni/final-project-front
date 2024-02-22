c177f556d6ca88434d6217e33796885210b2622d
Calin <velniciuc.calin@icloud.com>
2024-02-22 11:09:40 +0100

    feat | routes & navbar with conditional rendering (signUp/logIn of links to publishers and games)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
-import { useState } from 'react'
 import './App.scss'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import { useUser } from '../context/UserContext'
+import logo from './assets/react.svg'
+import GamePage from './pages/GamePage'
+import Games from './pages/Games'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import PublisherPage from './pages/PublisherPage'
+import Publishers from './pages/Publishers'
+import SignUser from './components/SignUser'
 
 function App() {
+
+  const { user } = useUser();
   
-  return (<>
-      
-  </>)
+  return (
+    <div className='app-wrapper'>
+      <nav className='main-navbar'>
+          <div className='nav-logo'>
+            <NavLink className={'navlink logo-link'} to={'/'} element={<HomePage/>}>
+              <figure className='logo-wrapper'>
+                <img src={logo} alt="IndieGames"/>
+              </figure>
+            </NavLink>
+          </div>
+          {user &&
+            <menu>
+              <li>
+                <NavLink className={'navlink'} to={'/publishers'} element={<Publishers/>}>Publishers</NavLink>
+              </li>
+              <li>
+                <NavLink className={'navlink'} to={'/games'} element={<Games/>}>Games</NavLink>
+              </li>
+            </menu>
+          }
+          {!user &&
+            <menu>
+              <li>
+                <NavLink className={'navlink'} to={'/signup'} element={<SignUser type='signup'/>}>Sign Up</NavLink>
+              </li>
+              <li>
+                <NavLink className={'navlink'} to={'/login'} element={<SignUser type='login'/>}>Log In</NavLink>
+              </li>
+            </menu>
+          }
+      </nav>
+
+      <Routes>
+        <Route path={'/'} element={<HomePage/>}/>
+        <Route path={'/signup'} element={<SignUser type='signup'/>}/>
+        <Route path={'/login'} element={<SignUser type='login'/>}/>
+        <Route path={'/publishers'}>
+          <Route index element={<Publishers/>}/>
+          <Route path={':slug'} element={<PublisherPage/>}/>
+        </Route>
+        <Route path={'/games'}>
+          <Route index element={<Games/>}/>
+          <Route path={':slug'} element={<GamePage/>}/>
+        </Route>
+        <Route path={'*'} element={<NotFound/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
