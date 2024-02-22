fdac1a93735ffc2f2fe9054ec7ec82e7a3bf9493
Calin <velniciuc.calin@icloud.com>
2024-02-22 17:09:34 +0100

    feat | collection & resource boilerplate for testing

--- src/pages/PublisherPage.jsx
+++ src/pages/PublisherPage.jsx
@@ -1,5 +1,47 @@
+import axios from "axios"
+import { axiosHeaders } from "../../library/utilities"
+import { useUser } from "../../context/UserContext"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+const { VITE_API_URL } = import.meta.env
+
 export default function () {
+
+    const { token } = useUser();
+    const { slug } = useParams()
+
+    const [publisher, setPublisher] = useState();
+    const [error, setError] = useState();
+    const blankFormData = {
+        name: '',
+        country: ''
+    };
+    const [formData, setFormData] = useState(blankFormData);
+    const [feedback, setFeedback] = useState();
+
+    useEffect(() => {
+        axios.get(`${VITE_API_URL}/publishers/${slug}`, axiosHeaders(token))
+            .then(obj => setPublisher(obj.data))
+            .catch(e => {
+                setError(e);
+                console.error(e)
+            })
+    }, []);
+
     return(<>
         <h1>Publisher Page</h1>
+        {publisher && 
+            <ul>
+                <li>
+                    Name: {publisher.name}
+                </li>
+                <li>
+                    Country: {publisher.country}
+                </li>
+                <li>
+                    Games: {publisher.games.map(g => ` ${g.title}, `)}
+                </li>
+            </ul>
+        }
     </>)
 }
--- src/pages/Publishers.jsx
+++ src/pages/Publishers.jsx
@@ -1,5 +1,86 @@
+import axios from "axios"
+import { axiosHeaders } from "../../library/utilities"
+import { useUser } from "../../context/UserContext"
+import { useEffect, useState } from "react"
+const { VITE_API_URL } = import.meta.env
+
 export default function () {
+
+    const { token } = useUser();
+
+    const [publishers, setPublishers] = useState();
+    const [error, setError] = useState();
+    const blankFormData = {
+        name: '',
+        country: ''
+    };
+    const [formData, setFormData] = useState(blankFormData);
+    const [feedback, setFeedback] = useState();
+
+    useEffect(() => {
+        axios.get(`${VITE_API_URL}/publishers`, axiosHeaders(token))
+            .then(obj => setPublishers(obj.data))
+            .catch(e => {
+                setError(e);
+                console.error(e)
+            })
+    }, []);
+
+    const addPublisher = (body) => {
+        axios.post(`${VITE_API_URL}/publishers`, body, axiosHeaders(token))
+            .then((obj) => {
+                setPublishers(obj.data);
+                setFeedback('Publisher added successfully.')
+            })
+            .catch(e => {
+                setError(e);
+                console.error(e)
+            })
+    };
+
     return(<>
         <h1>Publishers List</h1>
+        {publishers && 
+            publishers.map((pub) => {
+                return (
+                    <p key={pub._id}>{pub.name} | {pub.country}</p>
+                )
+            })
+        }
+        <div>
+            <h3>Add Publisher</h3>
+
+            <div>
+                <label> <b>Name:</b>
+                    <input
+                        type="text"
+                        value={formData.name}
+                        onChange={e => setFormData({
+                            ...formData,
+                            name: e.target.value
+                        })}
+                    />
+                </label>
+
+                <label> <b>Country:</b>
+                    <input
+                        type="text"
+                        value={formData.country}
+                        onChange={e => setFormData({
+                            ...formData,
+                            country: e.target.value
+                        })}
+                    />
+                </label>
+                <button
+                    onClick={() => {
+                        addPublisher(formData);
+                        setFormData(blankFormData)
+                    }}
+                >Add</button>
+            </div>
+
+            <p>{feedback}</p>
+        </div>
     </>)
 }
