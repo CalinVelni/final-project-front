de78f4a70d0a84b2eab777b8e156476464f93d94
Calin <velniciuc.calin@icloud.com>
2024-02-23 23:21:43 +0100

    feat | homepage updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 export default function () {
-    return(<>
+    return(
+    <div className="page">
         <h1 className="main-color center">Indie Games</h1>
-    </>)
+        <p className="home-p center">Welcome to Indie Games! <br/> A place where you can find the latest games released by indipendent developers all around the world. Have fun taking a look at them or became a developer youself and publish your own games!</p>
+    </div>
+    )
 }
